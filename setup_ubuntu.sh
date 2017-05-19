58f66ca8664b31038cef3bcde36737cac74e612e
jpcgt <jpcaram@gmail.com>
2017-05-19 23:22:32 +0000

    Cleanup.

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -1,6 +1,19 @@
 #!/bin/sh -e
+# "-e" exists on first error.
 
-apt-get install libpng-dev libfreetype6 libfreetype6-dev python-dev python-simplejson python-qt4 python-numpy python-scipy python-matplotlib libgeos-dev python-shapely python-pip libspatialindex-dev
+apt-get install libpng-dev \
+                libfreetype6 \
+				libfreetype6-dev \
+				python-dev \
+				python-simplejson \
+				python-qt4 \
+				python-numpy \
+				python-scipy \
+				python-matplotlib \
+				libgeos-dev \
+				python-shapely \
+				python-pip \
+				libspatialindex-dev
 easy_install -U distribute
 pip install --upgrade matplotlib Shapely
 pip install rtree
