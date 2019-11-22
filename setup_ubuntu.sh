da50b6e27aa13c7cb86bfef0d9f0c83e4751279a
Marius Stanciu <marius.adrian@gmail.com>
2019-11-22 03:45:54 +0200

    - wip

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt install --reinstall libpng-dev libfreetype6 libfreetype6-dev libgeos-dev libspatialindex-dev
 sudo apt install --reinstall python3-dev python3-pyqt5 python3-pyqt5.qtopengl python3-gdal python3-simplejson
-sudo apt install --reinstall python3-pip python3-tk
+sudo apt install --reinstall python3-pip python3-tk python3-imaging
 
 sudo python3 -m pip install --upgrade pip numpy scipy shapely rtree tk lxml cycler python-dateutil kiwisolver dill
 sudo python3 -m pip install --upgrade vispy pyopengl setuptools svg.path ortools freetype-py fontTools rasterio ezdxf
