9417049eefbb0dabb17af9fbc94ad499bcb54125
Marius Stanciu <marius.adrian@gmail.com>
2020-02-13 21:11:06 +0200

    - some updates in the requirements file and in setup_ubuntu.sh; for now versions higher than 5.12.1 of pyqt5 are not working

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -3,6 +3,7 @@ sudo apt install --reinstall libpng-dev libfreetype6 libfreetype6-dev libgeos-de
 sudo apt install --reinstall python3-dev python3-pyqt5 python3-pyqt5.qtopengl python3-gdal python3-simplejson
 sudo apt install --reinstall python3-pip python3-tk python3-imaging
 
+sudo python3 -m pip install --upgrade pyqt5==5.12
 sudo python3 -m pip install --upgrade pip numpy scipy shapely rtree tk lxml cycler python-dateutil kiwisolver dill
 sudo python3 -m pip install --upgrade vispy pyopengl setuptools svg.path ortools freetype-py fontTools rasterio ezdxf
 sudo python3 -m pip install --upgrade matplotlib qrcode reportlab svglib
