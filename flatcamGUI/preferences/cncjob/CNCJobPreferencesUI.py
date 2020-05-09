13296aea1df409d9b4dc58e91d0b37ba86b85c46
David Robertson <david@robertson.yt>
2020-05-09 19:31:09 +0100

    cleanup

--- flatcamGUI/preferences/cncjob/CNCJobPreferencesUI.py
+++ flatcamGUI/preferences/cncjob/CNCJobPreferencesUI.py
@@ -1,5 +1,3 @@
-from PyQt5 import QtWidgets
-
 from flatcamGUI.preferences.OptionsGroupUI import OptionsGroupUI
 from flatcamGUI.preferences.PreferencesSectionUI import PreferencesSectionUI
 from flatcamGUI.preferences.cncjob.CNCJobAdvOptPrefGroupUI import CNCJobAdvOptPrefGroupUI
@@ -18,14 +16,13 @@ class CNCJobPreferencesUI(PreferencesSectionUI):
 
     def __init__(self, decimals, **kwargs):
         self.decimals = decimals
-        self.cncjob_adv_opt_group = CNCJobAdvOptPrefGroupUI(decimals=self.decimals)
         super().__init__(**kwargs)
 
     def build_groups(self) -> [OptionsGroupUI]:
         return [
             CNCJobGenPrefGroupUI(decimals=self.decimals),
             CNCJobOptPrefGroupUI(decimals=self.decimals),
-            self.cncjob_adv_opt_group
+            CNCJobAdvOptPrefGroupUI(decimals=self.decimals)
         ]
 
     def get_tab_id(self):
