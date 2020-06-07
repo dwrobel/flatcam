cbaba7c60a5cb4a0a4162f6b92061bf40a36c259
Marius Stanciu <marius.adrian@gmail.com>
2020-06-07 19:41:54 +0300

    - minor fix

--- app_Main.py
+++ app_Main.py
@@ -381,7 +381,8 @@ class App(QtCore.QObject):
             f.close()
 
         # the factory defaults are written only once at the first launch of the application after installation
-        FlatCAMDefaults.save_factory_defaults(os.path.join(self.data_path, "factory_defaults.FlatConfig"), self.version)
+        FlatCAMDefaults.save_factory_defaults(os.path.join(self.data_path, "factory_defaults.FlatConfig"),
+                                              float(self.version))
 
         # create a recent files json file if there is none
         try:
